460a67ffd36b4533c215d819f58347090ab51016
sameer-khalili <sameer.khalili@student.hu.nl>
2020-04-28 19:08:09 +0200

    Tests toegevoegd

--- src/main/java/domain/rules/RockPaperScissors.java
+++ src/main/java/domain/rules/RockPaperScissors.java
@@ -4,35 +4,34 @@ import domain.Move;
 import domain.Result;
 import domain.Rules;
 
+
 public class RockPaperScissors implements Rules {
     @Override
     public Result decide(Move mine, Move theirs) {
-        switch (mine) {
-            case ROCK:
-                switch (theirs) {
-                    case SCISSORS:
-                        return Result.WIN;
-                    case PAPER:
-                        return Result.LOSE;
-                }
-
-            case PAPER:
-                switch (theirs) {
-                    case ROCK:
-                        return Result.WIN;
-                    case SCISSORS:
-                        return Result.LOSE;
-                }
-
-            case SCISSORS:
-                switch (theirs) {
-                    case PAPER:
-                        return Result.WIN;
-                    case ROCK:
-                        return Result.LOSE;
-                }
+        if (mine == theirs) {
+            return Result.DRAW;
         }
-
-        return Result.DRAW;
+        else{
+            switch (mine) {
+                case ROCK:
+                    switch (theirs) {
+                        case SCISSORS:
+                            return Result.WIN;
+                    }
+                    break;
+                case PAPER:
+                    switch (theirs) {
+                        case ROCK:
+                            return Result.WIN;
+                    }
+                    break;
+                case SCISSORS:
+                    switch (theirs) {
+                        case PAPER:
+                            return Result.WIN;
+                    }
+            }
+            return Result.LOSE;
+        }
     }
 }
